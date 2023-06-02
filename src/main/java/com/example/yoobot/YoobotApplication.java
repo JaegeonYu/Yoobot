43bfd497e51a1c4effd1e42d16dd9daa658f46dd
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2023-06-02 17:17:05 +0900

    Feat: Discord Token Crate

---
+++ src/main/java/com/example/yoobot/YoobotApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoobotApplication.class, args);
+    }
+
+}
